cc2ed9f852da59c5b3183a9568bdf3ce76a04924
Tim Ionescu <timionescu@gmail.com>
2023-03-10 00:47:54 +0200

    Add projects popup (edit/remove)

--- src/dom-display-project.js
+++ src/dom-display-project.js
@@ -1,5 +1,8 @@
 import elementFromHtml from './modal-form-build';
 import projectManager from './project-manager';
+import { buildTaskDomElement } from './dom-display-task';
+
+const mainAreaContent = document.querySelector('.main-area-content');
 
 export function buildProjectDomElement(parent) {
   const projects = projectManager.returnProjects();
@@ -7,12 +10,30 @@ export function buildProjectDomElement(parent) {
   const projectTitle = lastProjectAdded.getTitle();
 
   const project = elementFromHtml(`
-    <div class="project"><span class="hamburger">☰</span>${projectTitle}
-    <span class="dots">&#x22EE</span></div>
+    <div class="project-container">
+      <div class="project"><span class="hamburger">☰</span>${projectTitle}
+      <span class="dots">&#x22EE</span></div>
+      <div class="pop-up">
+        <div class="edit">Rename</div>
+        <div class="delete">Delete</div>
+      </div>
+    </div>
   `);
 
-  project.addEventListener('click', () => renderProject());
+  project.addEventListener('click', () => renderProject(lastProjectAdded));
   parent.appendChild(project);
+
+  const projectEdit = document.querySelector('.dots');
+  const popUp = document.querySelector('.pop-up')
+
+  projectEdit.addEventListener('click', () => {
+    console.log(popUp)
+    popUp.classList.toggle('popping')
+  });
 }
 
-function renderProject() {}
+function renderProject(project) {
+  mainAreaContent.innerHTML = '';
+  const projectTasks = project.getTasks();
+  buildTaskDomElement(mainAreaContent, projectTasks);
+}
