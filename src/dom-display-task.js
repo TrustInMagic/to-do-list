efe1efd73d605654adeada73ce1e126f04d99cbb
Tim Ionescu <timionescu@gmail.com>
2023-03-10 21:02:48 +0200

    Add details toggle to task DOM element

--- src/dom-display-task.js
+++ src/dom-display-task.js
@@ -9,26 +9,29 @@ export function buildTaskDomElement(parent, tasks) {
     let flagColor;
 
     let taskElement = elementFromHtml(`
-    <div class="task-body">
-      <input type="checkbox" class="task-check">
-      <span class="task-title">${taskTitle}</span>
-     <div class="task-due-date">${taskDueDate}</div>
-     <div class="flag-container${i}"></div>
-     <div class="task-dots">&#x22EE</div>
+    <div class="task-body task-body-${i}">
+      <input type="checkbox" class="task-check task-check-${i}">
+      <span class="task-title task-title-${i}">${taskTitle}</span>
+      <div class="task-details task-details-${i}">${taskDetails}</div>
+      <div class="right-container">
+        <div class="task-due-date task-due-date-${i}">${taskDueDate}</div>
+        <div class="flag-container flag-container-${i}"></div>
+        <div class="task-dots task-dots-${i}">&#x22EE</div>
+      </div>
     </div>
   `);
-    
+
     switch (taskPriority) {
       case 'low':
-        flagColor = 'blue'
+        flagColor = 'blue';
         break;
       case 'medium':
-        flagColor = 'orange'
-        break
+        flagColor = 'orange';
+        break;
       case 'high':
-        flagColor = 'red'
+        flagColor = 'red';
     }
-    
+
     let flag = elementFromHtml(`
     <?xml version="1.0" encoding="utf-8"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
       <svg width="22px" height="22px" viewBox="0 0 24 24" fill="${flagColor}" xmlns="http://www.w3.org/2000/svg">
@@ -36,10 +39,15 @@ export function buildTaskDomElement(parent, tasks) {
       <path d="M4 21V14M4 14V4H20L15 9L20 14H4Z" stroke="${flagColor}" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
     </svg>
   `);
-    
-        
-    parent.appendChild(taskElement)
-    const flagContainer = document.querySelector(`.flag-container${i}`)
-    flagContainer.appendChild(flag)
+
+    parent.appendChild(taskElement);
+    const flagContainer = document.querySelector(`.flag-container-${i}`);
+    const taskDomElement = document.querySelector(`.task-body-${i}`);
+    const detailsElement = document.querySelector(`.task-details-${i}`);
+
+    flagContainer.appendChild(flag);
+    taskDomElement.addEventListener('click', () =>
+      detailsElement.classList.toggle('show-details')
+    );
   }
 }
