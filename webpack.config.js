8295ee010a9b755f970a4ca91bae6f3e61521a35
Tim Ionescu <timionescu@gmail.com>
2023-03-18 11:32:08 +0200

    Edit file paths for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,10 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: '/src/index.js',
+  entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
   },
   devtool: 'inline-source-map',
   module: {
